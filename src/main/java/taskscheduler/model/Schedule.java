aeeb177fe3ac7da9127d2a98019c63e4d7ed814f
Hatem Ahmed <hatem.ahmed@pearson.com>
2018-08-24 20:30:05 -0400

    Adding total time and End time to the schedule

--- src/main/java/taskscheduler/model/Schedule.java
+++ src/main/java/taskscheduler/model/Schedule.java
@@ -1,5 +1,6 @@
 package taskscheduler.model;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -8,10 +9,12 @@ public class Schedule {
 
 	private LocalDateTime startTime;
 	private Queue<Consumer> schedule;
+	private Long totalTime;
 
-	private Schedule(LocalDateTime startTime, Queue<Consumer> schedule) {
+	private Schedule(LocalDateTime startTime, Queue<Consumer> schedule, Long totalTime) {
 		this.startTime = startTime;
 		this.schedule = schedule;
+		this.totalTime = totalTime;
 	}
 
 	public LocalDateTime getStartTime() {
@@ -22,13 +25,23 @@ public class Schedule {
 		return schedule;
 	}
 
+	public Long getTotalTime() {
+		return totalTime;
+	}
+
+	public LocalDateTime getEndTime() {
+		return startTime.plus(Duration.ofMillis(this.totalTime));
+	}
+
 	public static class ScheduleBuilder {
 		private LocalDateTime startTime;
 		private Queue<Consumer> consumers;
+		private Long totalTime;
 
 		public ScheduleBuilder() {
 			this.startTime = LocalDateTime.now();
 			this.consumers = new LinkedList<>();
+			this.totalTime = 0L;
 		}
 
 		public ScheduleBuilder startsAt(LocalDateTime startTime) {
@@ -38,11 +51,22 @@ public class Schedule {
 
 		public ScheduleBuilder withConsumers(Queue<Consumer> consumers) {
 			this.consumers = consumers;
+			setTotalTime(consumers);
 			return this;
 		}
 
+		private void setTotalTime(Queue<Consumer> consumers) {
+			Long totalTime = 0L;
+			for (Consumer consumer : consumers) {
+				if (consumer.getTimeElapsed() > totalTime) {
+					totalTime = consumer.getTimeElapsed();
+				}
+			}
+			this.totalTime = totalTime;
+		}
+
 		public Schedule build() {
-			return new Schedule(startTime, consumers);
+			return new Schedule(startTime, consumers, totalTime);
 		}
 	}
 }
